af8197d494f084ed5c3c29b11a41335d69776be5
Ziyad Sameh <ziyad.ss@hotmail.com>
2022-09-16 20:39:29 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,30 +2,21 @@
 
 #include "chess/Board.h"
 
-std::string squareName(chess::File f, chess::Rank r)
-{
-    char first = 'A' + f;
-    char second = '1' + r;
-    return std::string{first, second};
-}
-
 int main()
 {
     auto fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     auto board = chess::Board(fen);
 
-    constexpr const std::array<chess::File, 8> files = {chess::File::A, chess::File::B, chess::File::C, chess::File::D, chess::File::E,
-                                                        chess::File::F, chess::File::G, chess::File::H};
-    constexpr const std::array<chess::Rank, 8> ranks = {chess::Rank::One, chess::Rank::Two, chess::Rank::Three, chess::Rank::Four,
-                                                        chess::Rank::Five, chess::Rank::Six, chess::Rank::Seven, chess::Rank::Eight};
-    for (auto rank: ranks)
-    {
-        for (auto file: files)
-        {
-            auto moves = board.moves(file, rank);
-            std::cout << "Moves for " << squareName(file, rank) << ": " << moves << std::endl;
-        }
-    }
-
     std::cout << fen << std::endl << board.fen() << std::endl;
+
+    // {"b2b4", "a7a5", "b4a5", "a8a6", "a2a3", "a6h6", "a5a6", "h6g6", "a6a7", "g6h6", "a7a8k"};
+    auto moves = {"b2b4", "a7a5", "b4a5", "a8a6", "a2a3", "a6h6", "a5a6", "h6g6"};
+    for (auto move: moves) // Move is a 4 letter string, or 5 for promotions.
+    {
+        bool legal = board.move(move);
+        if (legal)
+            std::cout << "Legal move: " << move << ", fen: " << board.fen() << std::endl;
+        else
+            std::cout << "Illegal move: " << move << ", fen: " << board.fen() << std::endl;
+    }
 }
