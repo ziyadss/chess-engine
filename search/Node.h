35f0c08e90b003da9685656ef1ef606e6f4c0d08
Ziyad Sameh <ziyad.ss@hotmail.com>
2022-09-17 02:01:30 +0200

    Remove Node.cpp

--- search/Node.h
+++ search/Node.h
@@ -8,37 +8,77 @@ namespace search
     template<typename State, typename Action>
     class Node
     {
-        State *state;
+        State *m_state;
 
-        std::vector<Action *> path;
+        std::vector<Action *> m_path;
         // O(1) backtracking at cost of memory, can be reversed by using commented lines below instead
         // (along with similar changes in 'child_node' and 'backtrack' functions below)
         //Node *parent;
         //Action *action;
 
-        double pathCost;
-        double heuristicCost;
-        int insertionOrder;
+        double m_pathCost;
+        double m_heuristicCost;
+        int m_insertionOrder;
 
-        Node(const State *state, const std::vector<Action *> &path, double pathCost, double heuristicCost,
-             int insertionOrder) : state(state), path(path), pathCost(pathCost), heuristicCost(heuristicCost),
-                                   insertionOrder(insertionOrder) {}
+        constexpr Node(const State *state, const std::vector<Action *> &path, double pathCost, double heuristicCost, int insertionOrder)
+                : m_state(state), m_path(path), m_pathCost(pathCost), m_heuristicCost(heuristicCost), m_insertionOrder(insertionOrder) {}
 
     public:
-        explicit Node(const State& state) : state(&state), pathCost(0.0), heuristicCost(0.0), insertionOrder(0) {}
+        explicit constexpr Node(const State &state) : m_state(&state), m_pathCost(0.0), m_heuristicCost(0.0), m_insertionOrder(0) {}
 
-        bool operator<(const Node<State, Action> &other) const;
+        constexpr bool operator<(const Node<State, Action> &other) const
+        {
+            if (this->heuristicCost + this->pathCost == other.heuristicCost + other.pathCost)
+                return this->insertionOrder < other.insertionOrder;
+            return this->heuristicCost + this->pathCost < other.heuristicCost + other.pathCost;
+        }
 
-        bool operator<=(const Node<State, Action> &other) const;
+        constexpr bool operator<=(const Node<State, Action> &other) const
+        {
+            if (this->heuristicCost + this->pathCost == other.heuristicCost + other.pathCost)
+                return this->insertionOrder <= other.insertionOrder;
+            return this->heuristicCost + this->pathCost < other.heuristicCost + other.pathCost;
+        }
 
-        bool operator>(const Node<State, Action> &other) const;
+        constexpr bool operator>(const Node<State, Action> &other) const { return other < *this; }
 
-        bool operator>=(const Node<State, Action> &other) const;
+        constexpr bool operator>=(const Node<State, Action> &other) const { return other <= *this; }
 
-        std::vector<Action *> backtrack() const;
+        constexpr std::vector<Action *> backtrack() const
+        {
+            return this->path;
+            // O(1) backtracking at cost of memory, can be reversed by using commented lines below instead
+            // (along with similar changes in 'child_node' function and 'Node' class above)
 
-        Node<State, Action> *child(const Problem<State, Action> &problem, const Action *action, int order);
+            //std::vector<Action *> path;
+            //Node<State, Action> *node = this;
+            //while (node != nullptr)
+            //{
+            //    path.insert(path.begin(), node->action);
+            //    node = node->parent;
+            //}
+            //return path;
+        }
+
+        constexpr Node<State, Action> *child(const Problem<State, Action> &problem, const Action *action, int order) const
+        {
+            State *childState = problem.successor(*this->state, action);
+            std::vector<Action *> childPath = this->path;
+            childPath.push_back(&action);
+            double childPathCost = this->pathCost + problem.cost(this->state, action);
+            double childHeuristicCost = problem.heuristic(childState);
+            return new Node<State, Action>(childState, childPath, childPathCost, childHeuristicCost, order);
+        }
     };
 } // search
+
+template<typename State, typename Action>
+struct std::hash<search::Node<State, Action>>
+{
+    size_t operator()(const search::Node<State, Action> &node) const
+    {
+        return hash<State>()(node.state);
+    }
+};
 
 #endif //CHESS_ENGINE_NODE_H
