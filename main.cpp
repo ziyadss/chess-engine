4609c805eae1cae2d4cc4d0aac6d7c645640dd88
Ziyad Sameh <ziyad.ss@hotmail.com>
2022-10-29 11:05:27 +0200

    chess::Result fix

--- main.cpp
+++ main.cpp
@@ -14,8 +14,8 @@ int main()
         std::cout << board.fen() << std::endl << board.display() << std::endl << "Enter move: ";
         std::string move;
         std::cin >> move;
-        Result moveResult = board.move(move);
-        if (moveResult == Result::IllegalMove)
+        chess::Result moveResult = board.move(move);
+        if (moveResult == chess::Result::IllegalMove)
             std::cout << "Invalid move" << std::endl;
 
         std::cout << "moveResult = " << static_cast<int>(moveResult) << std::endl << std::endl;
@@ -24,7 +24,7 @@ int main()
     auto moves = {"a2a4", "a7a5", "b2b4", "b7b5", "b1c3", "b8c6", "c1a3", "c8a6", "d2d4", "d7d5", "d1d3", "d8d6", "e1c1"};
     for (auto move: moves) // Move is a 4 letter string, or 5 for promotions.
     {
-        bool legal = board.move(move) != Result::IllegalMove;
+        bool legal = board.move(move) != chess::Result::IllegalMove;
         if (legal)
             std::cout << "Legal move: " << move << ", fen: " << board.fen() << std::endl;
         else
